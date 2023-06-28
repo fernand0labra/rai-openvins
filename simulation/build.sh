3e5bd8cb5446ecca0a7d39a24ce68af05179a522
fernand0labra <fernandolabracaso@gmail.com>
2023-06-28 15:55:50 +0200

    Week 26 - Wednesday 28/06/2023

--- simulation/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cat ./Dockerfile.noetic-ros-core-focal | docker build -t noetic-ros-core-focal -
-
-cat ./Dockerfile.noetic-ros-base-focal | docker build -t noetic-ros-base-focal -
-
-cat ./Dockerfile.gzserver11-focal | docker build -t gzserver11-focal -
-
-cat ./Dockerfile.libgazebo11-focal | docker build -t ubuntu-focal-ros-noetic-gz11 -
---
+++ simulation/src/scripts/build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+cat ./Dockerfile.libgazebo11-focal | docker build -t ubuntu-focal-ros-noetic-gz11 -
+
+
+docker build -t rotors-simulator .
+
+
+docker run -it --net=host --gpus all \
+    --env="NVIDIA_DRIVER_CAPABILITIES=all" \
+    --env="DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    osrf/ros:noetic-desktop-full \
+    bash -it -c "roslaunch gazebo_ros empty_world.launch"
+
+docker run -it --net=host --gpus all \
+    --env="NVIDIA_DRIVER_CAPABILITIES=all" \
+    --env="DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    rotors-simulator \
+    bash
+
+rviz # you should be able to launch rviz once in bash
